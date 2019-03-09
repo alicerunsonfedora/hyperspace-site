b18cbf10c717ed150f72d9e337b831599f2eea2a
Marquis Kurt <software@marquiskurt.net>
2019-03-09 16:22:58 -0500

    Initial commit

---
+++ app/precache-manifest.1a1c4b2a565476a7caa6c6f204a641f1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "94c921230e0fadaac5a7",
+    "url": "./static/css/main.1f809275.chunk.css"
+  },
+  {
+    "revision": "ae7c5af0c35d94dbcd1c599fc7b400b7",
+    "url": "./static/media/manrope-semibold.ae7c5af0.otf"
+  },
+  {
+    "revision": "bef3ac94e3bdb0ccc783",
+    "url": "./static/js/1.bef3ac94.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "94c921230e0fadaac5a7",
+    "url": "./static/js/main.94c92123.chunk.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "8325e7ea6f0adcae48d1dbc6e45d1951",
+    "url": "./static/media/manrope-semibold.8325e7ea.woff"
+  },
+  {
+    "revision": "a00289fe0098f0dc10fcc3ece8073bb6",
+    "url": "./static/media/manrope-semibold.a00289fe.woff2"
+  },
+  {
+    "revision": "bef3ac94e3bdb0ccc783",
+    "url": "./static/css/1.d4fac4fe.chunk.css"
+  },
+  {
+    "revision": "63c565bad864d0ebee9137d03dc7b01e",
+    "url": "./index.html"
+  }
+];
